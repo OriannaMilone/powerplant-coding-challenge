c75f6a95825c8977482f4a31ec0e1d7777ccd4bc
Orianna Milone <oriannamilone.s@gmail.com>
2024-11-23 19:53:15 +0100

    cost function done

--- __init__.py
+++ __init__.py
@@ -6,9 +6,20 @@ app = Flask(__name__)
 
 @app.route('/energyproduction', methods=['POST'])
 def energyproduction():
-
-   #powerplants.selected_powerplants(load, gas_price, kerosine_price, wind, powerplants_list)
-   return True
+#    with open('/example_payloads/payload3.json') as f:
+#        data = json.load(f)
+   
+   data = request.json
+   load = data['load']
+   fuels = data['fuels']
+   gas_price = fuels['gas(euro/MWh)']
+   kerosine_price = fuels['kerosine(euro/MWh)']
+   wind = fuels['wind(%)']
+   powerplants_list = data['powerplants']
+    
+   powerplants_selected = powerplants.selected_powerplants(load, gas_price, kerosine_price, wind, powerplants_list)
+   
+   return powerplants_selected
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
--- powerplants.py
+++ powerplants.py
@@ -1,12 +1,30 @@
-import json
-
 result = []
 
 def selected_powerplants(load, gas_price, kerosine_price, wind, powerplants):
-    return result
+    merit_order = []
+    for powerplant in powerplants:
+        if powerplant["pmin"] > load:
+            result.append({"name": powerplant["name"], "p": 0})
+        else: 
+            merit_order.append(calculated_cost(powerplant, gas_price, kerosine_price, wind))         
+    merit_order = sorted(merit_order, key=lambda x: x["cost"])
+    #combined_powerplants(merit_order, load)
+    return merit_order
+    #return result
 
 
 def calculated_cost(powerplant, gas_price, kerosine_price, wind):
+    if powerplant["type"] == "gasfired":
+        cost = round(gas_price / powerplant["efficiency"], 2)
+        power_range = (powerplant["pmin"], powerplant["pmax"])
+        return ({"name": powerplant["name"], "power_range": power_range, "cost": cost,})
+    elif powerplant["type"] == "turbojet":
+        cost = round(kerosine_price / powerplant["efficiency"], 2)
+        power_range = (powerplant["pmin"], powerplant["pmax"])
+        return ({"name": powerplant["name"], "power_range": power_range, "cost": cost,})
+    else: 
+        power_range = (0, powerplant["pmax"] * wind / 100)
+        return ({"name": powerplant["name"], "power_range": power_range, "cost": 0,})
     return True 
     
 
